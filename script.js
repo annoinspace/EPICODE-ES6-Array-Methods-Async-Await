f3c9ec5156f400224eabb208a392a9eb51b839d8
annoinspace <aneesahalmask@gmail.com>
2022-11-09 15:31:33 +0000

    add to cart functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let container = document.getElementById("booksElement")
+let cartElement = document.getElementById("cart-text")
 
 const getBooks = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
@@ -14,16 +15,43 @@ const getBooks = () => {
 getBooks()
 
 function visualiseBook(book) {
-  console.log(book)
+  //   console.log(book)
   let card = document.createElement("div")
   card.classList.add("card", "col-3", "mb-3", "p-2")
   card.innerHTML = `<img src=${book.img} class="card-img-top" alt="...">
    <div class="card-body">
      <h5 class="card-title">${book.title}</h5>
      <p class="card-text">Category: ${book.category}</p>
-     <a href="#" class="btn btn-primary mb-2">Buy this for £${book.price}</a>
+     <div class="flex">
+     <button class="btn btn-primary mb-2 cart-btn" onclick="addToCart()">Add To Cart</button>
+     <span>Price: £${book.price}</span>
+     </div>
      <p class="card-text "> ID:${book.asin}</p>
     </div>
 `
   container.appendChild(card)
 }
+
+let bookToCartBtn = document.querySelectorAll(".cart-btn")
+
+let cartArray = []
+
+function addToCart() {
+  let btn = event.target
+
+  //   let title = btn.closest(".card-title")
+  //   title.classList.add("red-border")
+  let bookCard = btn.parentElement.parentElement.parentElement
+  bookCard.classList.add("red-border")
+  let title = bookCard.querySelector(".card-title").innerHTML
+  console.log(title)
+  alert(title + " - Added to Cart. ")
+  cartArray.push(title)
+  console.log(cartArray)
+  //   console.log("clicked")
+}
+
+// let bookToCartBtn = document.querySelector(
+//   "#booksElement > div:nth-child(1) > div > div > button"
+// )
+// bookToCartBtn.addEventListener("click", addToCart)
